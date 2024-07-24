2668e5eec629d9614ffc57b7366fb0e7a44619c5
derjogi <derjogi@gmail.com>
2024-07-24 21:46:30 +0200

    Bring back api/index.py which is necessary for vercel

---
+++ app/api/index.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+####
+# DO NOT DELETE this FILE or this FOLDER while using Vercel.
+# If this file is deleted, then Vercel will complain that there is no api
+# and fail to deploy. So this is a stub just to make vercel happy.
+# All real endpoints actually are forwarded to an external endpoint.
+####
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"Hello There! To process ideas, send a list of strings to the /process endpoint."}
