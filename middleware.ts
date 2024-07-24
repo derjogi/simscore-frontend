b6b210f1276765e1c166f27d8997f0fdff662e3c
derjogi <derjogi@gmail.com>
2024-07-24 22:44:50 +0200

    Will this timestamp (maybe?) solve multi-login-prompts?

--- middleware.ts
+++ middleware.ts
@@ -3,24 +3,24 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const basicAuth = request.headers.get('authorization')
-  console.log("Checking Auth")
+
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1]
     const [user, pwd] = atob(authValue).split(':')
-  
     const authUsers = new Map(
       Object.entries(JSON.parse(process.env.AUTH_USERS || '{}'))
     )
-
+    console.log(`Trying to log in with ${user.slice(0, 2)}${'*'.repeat(user.length - 2)}:${pwd.slice(0, 2)}${'*'.repeat(pwd.length - 2)}`)
     if (authUsers.get(user) && authUsers.get(user) === pwd) {
       return NextResponse.next()
     }
   }
 
+  const uniqueId = Date.now().toString();
   return new NextResponse('Authentication required', {
     status: 401,
     headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
+      'WWW-Authenticate': `Basic realm="Secure Area ${uniqueId}", charset="UTF-8"`,
     },
   })
 }
