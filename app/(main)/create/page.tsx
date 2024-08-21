51d78067f5a46a54450254314f99f852c1df7c5d
derjogi <derjogi@gmail.com>
2024-08-21 17:31:27 +0200

    Use onChange instead of onClick to keep the linter happy

--- app/(main)/create/page.tsx
+++ app/(main)/create/page.tsx
@@ -78,7 +78,7 @@ export default function Create() {
                 id="store-results"
                 type="checkbox"
                 checked={storeResults}
-                onClick={() => setStoreResults(!storeResults)}
+                onChange={() => setStoreResults(!storeResults)}
                 className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
               />
               <label
