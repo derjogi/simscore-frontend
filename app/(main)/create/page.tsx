64d37ecd2f60bdaf3690f98cfc69d1e8231fbff2
Jonas <derjogi@gmail.com>
2024-10-11 13:42:02 +1300

    Hide advanced idea input
    can be shown with ?advanced=true

--- app/(main)/create/page.tsx
+++ app/(main)/create/page.tsx
@@ -2,17 +2,18 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { PlotData, IdeasAndSimScores } from "@/constants";
 import Textarea from "react-dropzone-textarea";
-import LZString from 'lz-string';
+import LZString from "lz-string";
+import { useSearchParams } from 'next/navigation';
 
 export default function Create() {
   const [input, setInput] = useState("");
   const [storeResults, setStoreResults] = useState(false);
-  const [output, setOutput] = useState<IdeasAndSimScores>();
-  const [plotData, setPlotData] = useState<PlotData>();
   const [isLoading, setIsLoading] = useState(false);
-  const [id, setId] = useState("");
+  const [_, setId] = useState("");
+
+  const searchParams = useSearchParams();
+  const showIdeaInput = searchParams.get('advanced') == 'true';
 
   const router = useRouter();
 
@@ -57,17 +58,17 @@ export default function Create() {
     singlePointFocus?: IndividualRating;
   };
 
-type IndividualRating = {
-  quality: number
-  message: string
-}
+  type IndividualRating = {
+    quality: number;
+    message: string;
+  };
 
-type IdeaRating = {
-  rating: {
-    singlePointFocus: IndividualRating
-    sentiment: IndividualRating
-  }
-}
+  type IdeaRating = {
+    rating: {
+      singlePointFocus: IndividualRating;
+      sentiment: IndividualRating;
+    };
+  };
 
   // For individual input fields:
   const [ideas, setIdeas] = useState<string[]>([""]);
@@ -108,12 +109,13 @@ type IdeaRating = {
     }
   };
 
-  const validateFast = (index: number, idea: string) =>{
+  const validateFast = (index: number, idea: string) => {
     console.log(`Validating idea ${index}: `, idea);
     const newIdeaValidity = [...ideaValidity];
-    const lengthValidity = idea.trim().length < 60
-      ? "Idea must be at least 60 characters long."
-      : idea.trim().length > 150
+    const lengthValidity =
+      idea.trim().length < 60
+        ? "Idea must be at least 60 characters long."
+        : idea.trim().length > 150
         ? "Idea must be at most 150 characters long."
         : "";
 
@@ -124,7 +126,7 @@ type IdeaRating = {
     setIdeaValidity(newIdeaValidity);
     console.log("Idea validity: ", newIdeaValidity);
     return lengthValidity;
-  }
+  };
 
   function queryForIdeaValidity(idea: string, index: number) {
     const host = process.env.SIMSCORE_API;
@@ -167,10 +169,10 @@ type IdeaRating = {
     const validity = ideaValidity[index];
     if (!validity) return null;
     if (validity.length) return validity.length;
-    if (validity.sentiment && validity.sentiment.quality < 5 ) {
+    if (validity.sentiment && validity.sentiment.quality < 5) {
       return validity.sentiment.message;
     }
-    if (validity.singlePointFocus && validity.singlePointFocus.quality < 5 ) {
+    if (validity.singlePointFocus && validity.singlePointFocus.quality < 5) {
       return validity.singlePointFocus.message;
     }
 
@@ -179,95 +181,92 @@ type IdeaRating = {
 
   return (
     <>
-      {!plotData && (
-        <div className="space-y-4">
-          {ideas.map((idea, index) => (
-            <>
-              <div key={index} className="flex items-center space-x-2">
-                <input
-                  type="text"
-                  value={idea}
-                  onChange={(e) => updateIdea(index, e.target.value)}
-                  onKeyUp={(e) => addNewIdeaOnEnter(e)}
-                  onBlur={(e) => validateIdea(index, e.target.value)}
-                  className={`flex-grow p-2 border rounded-lg ${
-                    getValidityError(index) ? "border-red-500" : "border-gray-300"
-                  }`}
-                  placeholder="Enter an idea"
-                  autoFocus={index === ideas.length - 1}
-                />
-                {index === ideas.length - 1 && (
-                  <button
-                    onClick={addEmptyIdea}
-                    className="p-2 bg-blue-500 text-white rounded-lg"
-                  >
-                    +
-                  </button>
-                )}
-                {ideas.length > 1 && (
-                  <button
-                    onClick={() => removeIdea(index)}
-                    className="p-2 bg-red-500 text-white rounded-lg"
-                  >
-                    -
-                  </button>
-                )}
-              </div>
-              {getValidityError(index) && (
-                <p className="text-red-500 text-sm">
-                  {getValidityError(index)}
-                </p>
-              )}
-            </>
-          ))}
-
-          <form className="space-y-2" onSubmit={handleSubmit}>
-            <label
-              htmlFor={"answer"}
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Type or upload your answer(s). Separate them by ⏎ (new lines)
-            </label>
-            <Textarea
-              id="answer"
-              value={input}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setInput(e.target.value)
-              }
-              onDropRead={(text: string) => setInput(text)}
-              textareaProps={{
-                cols: 80,
-                rows: 8,
-                placeholder: "Enter your answers, or upload a file here...",
-                className: "!bg-white border-2 rounded-lg p-2",
-              }}
-            />
-            <div className="flex items-center mb-4">
+      <div className="space-y-4">
+        {/* This is an advanced input field, by default */}
+        {showIdeaInput && ideas.map((idea, index) => (
+          <>
+            <div key={index} className="flex items-center space-x-2">
               <input
-                id="store-results"
-                type="checkbox"
-                checked={storeResults}
-                onChange={() => setStoreResults(!storeResults)}
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                type="text"
+                value={idea}
+                onChange={(e) => updateIdea(index, e.target.value)}
+                onKeyUp={(e) => addNewIdeaOnEnter(e)}
+                onBlur={(e) => validateIdea(index, e.target.value)}
+                className={`flex-grow p-2 border rounded-lg ${
+                  getValidityError(index) ? "border-red-500" : "border-gray-300"
+                }`}
+                placeholder="Enter an idea"
+                autoFocus={index === ideas.length - 1}
               />
-              <label
-                htmlFor="store-results"
-                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Store results to make them shareable 
-              </label>
+              {index === ideas.length - 1 && (
+                <button
+                  onClick={addEmptyIdea}
+                  className="p-2 bg-blue-500 text-white rounded-lg"
+                >
+                  +
+                </button>
+              )}
+              {ideas.length > 1 && (
+                <button
+                  onClick={() => removeIdea(index)}
+                  className="p-2 bg-red-500 text-white rounded-lg"
+                >
+                  -
+                </button>
+              )}
             </div>
+            {getValidityError(index) && (
+              <p className="text-red-500 text-sm">{getValidityError(index)}</p>
+            )}
+          </>
+        ))}
 
-            <button
-              type="submit"
-              onClick={handleSubmit}
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        <form className="space-y-2" onSubmit={handleSubmit}>
+          <label
+            htmlFor={"answer"}
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Type or upload your answer(s). Separate them by ⏎ (new lines)
+          </label>
+          <Textarea
+            id="answer"
+            value={input}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInput(e.target.value)
+            }
+            onDropRead={(text: string) => setInput(text)}
+            textareaProps={{
+              cols: 80,
+              rows: 8,
+              placeholder: "Enter your answers, or upload a file here...",
+              className: "!bg-white border-2 rounded-lg p-2",
+            }}
+          />
+          <div className="flex items-center mb-4">
+            <input
+              id="store-results"
+              type="checkbox"
+              checked={storeResults}
+              onChange={() => setStoreResults(!storeResults)}
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              htmlFor="store-results"
+              className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
-              Process
-            </button>
-          </form>
-        </div>
-      )}
+              Store results to make them shareable
+            </label>
+          </div>
+
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Process
+          </button>
+        </form>
+      </div>
 
       {isLoading && (
         <div className="flex justify-center items-center">
