c3a434da0f06e11a49c4b939659e435e5a7a78ad
Jonas <derjogi@gmail.com>
2024-10-26 17:58:05 +1300

    Hotfix - trying to get session fetching working

--- app/api/session/[id]/route.ts
+++ app/api/session/[id]/route.ts
@@ -5,8 +5,9 @@ export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
+  console.log("Received request with id:", params.id);
   const id = params.id;
-
+  console.log("Step 1");
   // Initialize MongoDB client (you may want to use a connection pool or singleton)
   const client = new MongoClient(process.env.MONGODB_URI as string);
 
@@ -14,11 +15,11 @@ export async function GET(
     await client.connect();
     const db = client.db("SimScore");
     const collection = db.collection("Sessions");
-
+    console.log("Step 2");
     // Convert string id to ObjectId
     const objectId = new ObjectId(id);
     const document = await collection.findOne({ _id: objectId });
-
+    console.log("Step 3");
     if (document) {
       console.log("Found document")
       
@@ -28,6 +29,7 @@ export async function GET(
       console.log(result)
       return NextResponse.json(result);
     } else {
+      console.log("Step 4 - not found");
       console.log("Document with id not found: ", id)
       return NextResponse.json({ error: "Document not found" }, { status: 404 });
     }
